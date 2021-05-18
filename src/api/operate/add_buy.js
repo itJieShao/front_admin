e1e035635b2ddc4e6f89894d106e429208354cc2
chenshaojie <609815478@qq.com>
2021-05-18 11:46:52 +0800

    commit

--- src/api/operate/add_buy.js
+++ src/api/operate/add_buy.js
@@ -9,6 +9,15 @@ export function getList(data) {
     })
 }
 
+//详情
+export function getDetail(data) {
+    return request({
+        url: '/product/vendor-menu-purchased/detail',
+        method: 'post',
+        data
+    })
+}
+
 //禁用/启用
 export function updateStatus(data) {
     return request({
